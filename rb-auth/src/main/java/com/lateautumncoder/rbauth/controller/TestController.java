69c83ca1cf0c696bf05f61a19fffe2c0d0b297b6
GlobalXiaoHua <409732463@qq.com>
2024-10-20 19:50:18 +0800

    整合 SaToken 权限认证框架，以及初步尝鲜

--- rb-auth/src/main/java/com/lateautumncoder/rbauth/controller/TestController.java
+++ rb-auth/src/main/java/com/lateautumncoder/rbauth/controller/TestController.java
@@ -1,12 +1,10 @@
 package com.lateautumncoder.rbauth.controller;
 
+import cn.dev33.satoken.stp.StpUtil;
 import com.lateautumncoder.aspect.ApiOperationLog;
 import com.lateautumncoder.response.Response;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 
@@ -34,4 +32,23 @@ public class TestController {
     public Response<User> test3(@Validated @RequestBody User user) {
         return Response.success(user);
     }
+
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("/user/doLogin")
+    public String doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if ("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+            return "登录成功";
+        }
+        return "登录失败";
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("/user/isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+
+
 }
